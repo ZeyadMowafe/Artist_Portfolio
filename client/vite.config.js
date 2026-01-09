e3c733df104f683ee23cf4bdb758a1e250894889
Zeyad Ahmed Mowafe <zezomowafe2@gmail.com>
2026-01-09 20:48:41 +0200

    Set base path for Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
+  base: '/Artist_Portfolio/',
   plugins: [react()],
   css: {
     postcss: {
